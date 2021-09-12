ec4d03861a15c0499687ba042e40293e5156a6d6
hellonayeon <hellonayeon@icloud.com>
2021-09-12 21:26:58 +0900

    기사 스크래핑 내용 엑셀 파일에 저장하기

--- NewsScraping/new_scraping.py
+++ NewsScraping/new_scraping.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from openpyxl import Workbook
 
 driver = webdriver.Chrome('./chromedriver')
 
@@ -10,7 +11,13 @@ req = driver.page_source
 soup = BeautifulSoup(req, 'html.parser')
 
 articles = soup.select('#main_pack > section.sc_new.sp_nnews._prs_nws > div > div.group_news > ul > li')
-print(articles)
+# 출력해보면 어디까지 추출했는지 확인 가능
+# print(articles)
+
+# [엑셀에 스크래핑한 내용 저장] 워크북, 워크시트 생성
+wb = Workbook()
+ws = wb.active
+ws.title = "articles"
 
 for article in articles:
     # 'li' 태그의 하위에서 다시 찾기
@@ -19,6 +26,7 @@ for article in articles:
     url = article.select_one('div.news_wrap.api_ani_send > div.news_area > a')['href']
     comp = article.select_one('div.news_wrap.api_ani_send > div.news_area > div.news_info > div.info_group > a').text.split(" ")[0].replace('언론사', '')
 
-    print(title, url, comp)
+    ws.append([title, url, comp]) # 엑셀 파일에 저장할 내용 추가
 
 driver.quit()
+wb.save(filename='article.xlsx')
